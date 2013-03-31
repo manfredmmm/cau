3c30fb56ea7648199abb0b6f0cd95e4d6fb5b022
manfred <manfred@imesmes.com>
2013-03-31 20:32:23 +0200

    Added dynamic information

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @news = Section.find_by_name("news")
+    @aboutus = Section.find_by_name("aboutus")
+    @units = Section.find_by_name("units")
   end
 end
