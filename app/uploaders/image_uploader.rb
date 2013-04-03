b8cb676a002ecdbc01f647b5809b22153a8f1b12
manfred <manfred@imesmes.com>
2013-04-04 00:07:15 +0200

    Reestyling content and images uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
@@ -28,7 +28,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  # process :scale => [350, 225]
+  
+  process :resize_to_fit => [450, 275]
+
   #
   # def scale(width, height)
   #   # do something
