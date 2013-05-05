8d6ff8b8e3d875557d7d4521067aa7dbb65e8bf4
manfred <manfred@imesmes.com>
2013-05-05 13:59:29 +0200

    Added new caps section and updates on news

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
     @news = Section.find_by_name("news")
     @aboutus = Section.find_by_name("aboutus")
     @units = Section.find_by_name("units")
+    @caps = Section.find_by_name("caps")
   end
 end
